7d940f2c642a2b32c11f2d9d42f30acdbbceb141
jamz9714 <jamz97114@gmail.com>
2023-09-18 13:35:44 +0800

    918mzw2

--- 9136/ass2 - copy/Assignment 2 Template  Data/RENAME-ME/main.py
+++ 9136/ass2 - copy/Assignment 2 Template  Data/RENAME-ME/main.py
@@ -1,13 +1,37 @@
 # your imports goes here
-
+from car_retailer import CarRetailer
+from car import Car
+from retailer import Retailer
+from order import Order
 def main_menu():
-	pass
+	print("""a) Look for the nearest car retailer
+b) Get car purchase advice
+c) Place a car order
+d) Exit""")
 
 def generate_test_data():
-	pass
+	for i in range(3):
+		
+		retailer_id = generate_retailer_id()
 
 def main():
-	pass
+	while True:
+		main_menu()
+		menu_choice= input('Please input your choice (a,b,c,d):')
+		if menu_choice.lower() == 'a':
+			customer_postcode = int(input('Please input your postcode:'))
+			CarRetailer.get_postcode_distance(customer_postcode)
+			
+			pass
+		elif menu_choice.lower() == 'b':
+			pass
+		elif menu_choice.lower() == 'c':
+			pass
+		elif menu_choice.lower() == 'd':
+			break
+		else:
+			continue
+
 
 if __name__ == "__main__":
-	pass
+	main()
